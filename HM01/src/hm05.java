aa25eff727e73d3cc4965fad05cadb027626f74a
Cuhao <106021385@live.asia.edu.tw>
2017-09-30 15:58:45 +0800

    4245
    455454

---
+++ HM01/src/hm05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hm05 {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int a = scn.nextInt();
+    int b = scn.nextInt();
+
+    System.out.println(Math.round(a/0.454f) );
+    System.out.println(Math.round(b/2.54f));
+
+   }
+}
